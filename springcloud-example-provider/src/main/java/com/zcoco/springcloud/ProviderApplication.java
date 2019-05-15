98099b61f8a30af835b6b5140fff44c96e117778
zcoco <zheng_coco@yeah.net>
2019-05-15 13:16:50 +0800

    集成eureka

--- springcloud-example-provider/src/main/java/com/zcoco/springcloud/ProviderApplication.java
+++ springcloud-example-provider/src/main/java/com/zcoco/springcloud/ProviderApplication.java
@@ -1,22 +1,22 @@
 package com.zcoco.springcloud;
 
-/*
- * 描述: Consumer 启动引导
- * @auth zhengkk19738
- * @time 2019/5/15
- */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/*
+ * 描述:
+ * @auth zhengkk19738
+ * @time 2019/5/15
+ */
+
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class ConsumerBootstrap {
+@EnableEurekaClient
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerBootstrap.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
