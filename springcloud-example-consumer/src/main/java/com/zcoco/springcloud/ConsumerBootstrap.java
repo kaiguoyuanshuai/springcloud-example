8aad40a081b31a378bd34afd23780a867a762139
zcoco <zheng_coco@yeah.net>
2019-05-15 12:54:12 +0800

    init project

---
+++ springcloud-example-consumer/src/main/java/com/zcoco/springcloud/ConsumerBootstrap.java
@@ -0,0 +1,16 @@
+package com.zcoco.springcloud;
+
+/*
+ * 描述: Consumer 启动引导
+ * @auth zhengkk19738
+ * @time 2019/5/15
+ */
+
+import org.springframework.boot.SpringApplication;
+
+public class ConsumerBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerBootstrap.class, args);
+    }
+}
---
+++ springcloud-example-eureka/src/main/java/com/zcoco/springcloud/Application.java
@@ -0,0 +1,19 @@
+package com.zcoco.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ * 描述:
+ * @auth zhengkk19738
+ * @time 2019/5/15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
